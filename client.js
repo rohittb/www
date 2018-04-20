7e53c255e67d8d4f09468b5cbacf75829b5800e6
rohit <rohittb@gmail.com>
2018-04-20 16:14:15 +0530

    xcsds

---
+++ client.js
@@ -0,0 +1,19 @@
+var socket = io.connect( 'http://172.31.20.146:8080' );
+
+$( "#SendButton" ).click( function() {
+	var msg = $( "#messageText" ).val();
+	
+	socket.emit( 'message', { message: msg } );
+	return false;
+});
+
+socket.on( 'message', function( data ) {
+	var actualContent = $( "#incomingMessages" ).html();
+	var newMsgContent = '<li>' + data.message + '</li>';
+	var content = actualContent+newMsgContent;
+	$( "#incomingMessages" ).html( content );
+	$('#messageText').val('');
+	var objDiv = document.getElementById("incomingMessages");
+	objDiv.scrollTop = objDiv.scrollHeight;
+			
+});
